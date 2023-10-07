a2df32f1c4d30b652b015fb12c377f330afd3173
gabrielxoko <146871731+gabrielxoko@users.noreply.github.com>
2023-10-08 00:47:40 +0200

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCOmTnd8Md2yK0SVEwI1l6VbV1M7N8ySaA",
+//   authDomain: "magneti1001.firebaseapp.com",
+//   projectId: "magneti1001",
+//   storageBucket: "magneti1001.appspot.com",
+//   messagingSenderId: "596941070186",
+//   appId: "1:596941070186:web:e9af3e1e5ecb7459e80f13",
+//   measurementId: "G-Z02PPN0HRS"
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQLkuYxfSqkdcfsaSQ9_5nUU859AJXegA",
+  authDomain: "meta-caused.firebaseapp.com",
+  projectId: "meta-caused",
+  storageBucket: "meta-caused.appspot.com",
+  messagingSenderId: "547960107250",
+  appId: "1:547960107250:web:62c8d86abf3105a5a8e06b"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+// Export firestore database
+// It will be imported into your react app whenever it is needed
+export const db = getFirestore(app);
